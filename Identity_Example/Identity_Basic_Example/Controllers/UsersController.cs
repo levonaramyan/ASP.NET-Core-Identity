16736975a06e32466128c4b21cbe1d07614c8015
Levon Aramyan <levonaramyan@gmail.com>
2019-08-13 18:21:51 +0400

    Added change the password action

--- Identity_Example/Identity_Basic_Example/Controllers/UsersController.cs
+++ Identity_Example/Identity_Basic_Example/Controllers/UsersController.cs
@@ -116,5 +116,53 @@ namespace Identity_Basic_Example.Controllers
             return RedirectToAction("Index");
         }
 
+        public async Task<IActionResult> ChangePassword(string id)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            ChangePasswordViewModel model = new ChangePasswordViewModel
+            {
+                Email = user.Email,
+                Id = user.Id
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByIdAsync(model.Id);
+
+                if (user != null)
+                {
+                    var result = await _userManager.ChangePasswordAsync(user,model.OldPassword,model.NewPassword);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty,error.Description);
+                        }
+                    }
+                } else
+                {
+                    ModelState.AddModelError(string.Empty, "Օգտատերը չի գտնվել");
+                }
+            }
+
+            return View(model);
+        }
     }
 }
