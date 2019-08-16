3565e7446b94fd46e2f6dddf42c85fe00ca6e3eb
Levon Aramyan <levonaramyan@gmail.com>
2019-08-16 12:23:01 +0400

    Added RoleInitializer

---
+++ Identity_Example/Identity_Basic_Example/Helpers/RoleInitializer.cs
@@ -0,0 +1,38 @@
+﻿using Identity_Basic_Example.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity_Basic_Example.Helpers
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string adminEmail = "qwerty@test.com";
+            string password = "Qwerty123!";
+
+            if(await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+            if (await roleManager.FindByNameAsync("user") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("user"));
+            }
+            if (await userManager.FindByEmailAsync(adminEmail) == null)
+            {
+                User applicationAdmin = new User { Email = adminEmail, UserName = adminEmail };
+
+                var result = await userManager.CreateAsync(applicationAdmin, password);
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(applicationAdmin,"admin");
+                }
+            }
+        }
+    }
+}
--- Identity_Example/Identity_Basic_Example/Program.cs
+++ Identity_Example/Identity_Basic_Example/Program.cs
@@ -3,18 +3,41 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Identity_Basic_Example.Helpers;
+using Identity_Basic_Example.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Identity_Basic_Example
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            //CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var userManager = services.GetRequiredService<UserManager<User>>();
+                    var rolesManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    await RoleInitializer.InitializeAsync(userManager, rolesManager);
+                }
+                catch (Exception ex)
+                {
+                    //var logger = services.GetRequiredService<ILogger<Program>>();
+                    //logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
